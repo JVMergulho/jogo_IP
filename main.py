e32def09b47f565dc00a1b1f375dbcc62cae0cb4
JVMergulho <jvitormergulho@gmail.com>
2022-10-14 10:28:58 -0300

    Update main

--- main.py
+++ main.py
@@ -5,38 +5,37 @@ from player import Player
 from item import *
 
 
-def gerar_itens(screen,all_items):
-    qtd = random.randint(1, 5)
-    num_item = random.randint(0,1)
+def gerar_itens(screen, all_items):
+    num = random.randint(1, 5)
 
-    for i in range(qtd):
-        if num_item==1:
-            item = Coffee()
-        else:
-            item =Energy_drink()
+    for i in range(num):
+        item = Item(screen)
         all_items.add(item)
 
+
 def main():
-    screen= pg.display.set_mode((672,672))
-    clock= pg.time.Clock()
+    screen = pg.display.set_mode((672, 672))
+    clock = pg.time.Clock()
     pg.display.set_caption('Bug Bounty')
 
     random.seed()
 
-    player= Player(screen,320,320,pg.K_d,pg.K_a,pg.K_w,pg.K_s)
-    #player2=Player(screen,300,320,pg.K_RIGHT,pg.K_LEFT,pg.K_UP,pg.K_DOWN)
+    player = Player(screen, 320, 320, pg.K_d, pg.K_a, pg.K_w, pg.K_s)
+    # player2=Player(screen,300,320,pg.K_RIGHT,pg.K_LEFT,pg.K_UP,pg.K_DOWN)
     #player3 = Player(screen, 250, 320, pg.K_k, pg.K_h, pg.K_u, pg.K_j)
 
-    all_sprites= pg.sprite.Group()
+    all_sprites = pg.sprite.Group()
     all_items = pg.sprite.Group()
 
-    gerar_itens(screen,all_items)
+    gerar_itens(screen, all_items)
 
     all_sprites.add(player)
+    # all_sprites.add(player2)
+    # all_sprites.add(player3)
 
     while True:
         for event in pg.event.get():
-            if event.type== pg.QUIT:
+            if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
 
