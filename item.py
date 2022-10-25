ea9e7d7f3720e8b47efc20f7136cc0422274bd55
JVMergulho <jvitormergulho@gmail.com>
2022-10-25 16:58:26 -0300

    Tamanho da moeda alterado e som do bug adicionado

--- item.py
+++ item.py
@@ -1,20 +1,20 @@
 import pygame as pg
 import random
 
+
 class Item(pg.sprite.Sprite):
-    def __init__(self, tipo, imagem, player, itens_lista,x,y):
+    def __init__(self, tipo, imagem, player, itens_lista, x, y):
 
         pg.sprite.Sprite.__init__(self)
 
         # define o tipo do item: coffee ou energy_drink
         self.type = tipo
-        
 
         # define a sprite que vai ser usada
         self.image = imagem
 
         self.rect = self.image.get_rect()
-        
+
         self.x = x
         self.y = y
 
@@ -22,17 +22,22 @@ class Item(pg.sprite.Sprite):
         self.itens_lista = itens_lista
 
         # configura a dimensão da imagem e do rect
-        self.image = pg.transform.scale(self.image, (30, 30))
+        if tipo == 'bit_1' or tipo == 'bit_0':
+            # o tamanho da moeda é um pouco maior
+            self.image = pg.transform.scale(self.image, (40, 40)) 
+        else:
+            self.image = pg.transform.scale(self.image, (30, 30))
+    
         self.rect.update(self.x, self.y, 20, 20)
 
-        self.time=0
+        self.time = 0
 
     def update(self):
 
         # Faz os itens desaparecerem depois de certo tempo
-        self.time+=1
+        self.time += 1
 
-        if self.time>150:
+        if self.time > 150:
             pg.sprite.Sprite.kill(self)
 
             self.itens_lista.remove(self)
@@ -40,7 +45,7 @@ class Item(pg.sprite.Sprite):
         # Caso o player toque no item
         if self.rect.colliderect(self.player.rect):
             pg.sprite.Sprite.kill(self)
-            
+
             self.itens_lista.remove(self)
 
             return self.type
--- main.py
+++ main.py
@@ -10,28 +10,29 @@ from item import *
 from projectile import Projectile
 import math
 
-def gerar_itens(itens_lista, all_items, player,x,y):
-    num = random.randint(1, 5)
+
+def gerar_itens(itens_lista, all_items, player, x, y):
 
     imagens_itens = {'coffee': pg.image.load('assets\cafe.gif'),
-                     'energy_drink': pg.image.load('assets\energy_drink.png'),        
+                     'energy_drink': pg.image.load('assets\energy_drink.png'),
                      'bit_1': pg.image.load('assets\\bit_1.png'),
-                     'bit_0' : pg.image.load('assets\\bit_0.png')}  
-    
+                     'bit_0': pg.image.load('assets\\bit_0.png')}
+
     for i in range(1):
 
         if random.randint(0, 3) == 0:
             tipo = 'coffee'
-        elif random.randint(0 , 3) == 1:
+        elif random.randint(0, 3) == 1:
             tipo = 'bit_0'
         elif random.randint(0, 3) == 2:
             tipo = 'bit_1'
         else:
             tipo = 'energy_drink'
 
-        item = Item(tipo, imagens_itens[tipo], player, itens_lista,x,y)
+        item = Item(tipo, imagens_itens[tipo], player, itens_lista, x, y)
         itens_lista.append(item)
         all_items.add(item)
+
 
 def main():
 
@@ -39,14 +40,17 @@ def main():
     pg.init()
 
     pg.mixer.music.load('assets\game_music.mp3')
+    pg.mixer.music.set_volume(0.7)
     pg.mixer.music.play(-1)
 
     # sons do jogo
-    item_sound = pg.mixer.Sound(Path('assets','item_sound.flac'))
+    item_sound = pg.mixer.Sound(Path('assets', 'item_sound.flac'))
     item_sound.set_volume(0.3)
 
-    spray_sound = pg.mixer.Sound(Path('assets','spray_sound.wav'))
+    spray_sound = pg.mixer.Sound(Path('assets', 'spray_sound.wav'))
     spray_sound.set_volume(0.3)
+
+    morte_bug_sound = pg.mixer.Sound(Path('assets', 'morte_bug.wav'))
 
     # define a variárvel que armazena o padrão RGB para a cor branca
     branco = (255, 255, 255)
@@ -59,7 +63,7 @@ def main():
 
     # inicia o objeto que será a tela do jogo
     screen = pg.display.set_mode((672, 672))
-    width,height = 672,672
+    width, height = 672, 672
     clock = pg.time.Clock()
     pg.display.set_caption('Bug Bounty')
 
@@ -67,7 +71,7 @@ def main():
 
     itens_lista = []
 
-    all_sprites= pg.sprite.Group()
+    all_sprites = pg.sprite.Group()
     all_items = pg.sprite.Group()
     #all_bugs = pg.sprite.Group()
     spri_bugs = pg.sprite.Group()
@@ -93,19 +97,19 @@ def main():
 
     all_sprites.add(player)
 
-    #variavel para controlar o spaw dos bugs
+    # variavel para controlar o spaw dos bugs
     contador = 0
-    #variavel para nao permitir atirar varias vezes ao mesmo tempo
+    # variavel para nao permitir atirar varias vezes ao mesmo tempo
     cooldown = 12
 
     while True:
-        cooldown += 1 #esfriar o inseticida]
-    
+        cooldown += 1  # esfriar o inseticida]
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
-            
+
             #
             if event.type == pg.MOUSEBUTTONDOWN and cooldown >= 12:
                 bala = Projectile(player)
@@ -113,9 +117,9 @@ def main():
                 cooldown = 0
 
                 spray_sound.play()
-                
-        #      
-        for balas in all_bullets: #movimento do gas na tela
+
+        #
+        for balas in all_bullets:  # movimento do gas na tela
             balas.projectile_move()
         #
 
@@ -129,29 +133,30 @@ def main():
         all_sprites.update()
 
         all_items.draw(screen)
-        
-        if contador%150 == 0:
+
+        if contador % 150 == 0:
             for i in range(4):
                 x_left = random.randint(-40, -10)
-                x_right = random.randint(690,720)
+                x_right = random.randint(690, 720)
                 x = choice([x_left, x_right])
-                y = random.randint(50,600)
-                bug = Bug(x,y)
-                #all_bugs.add(bug)
+                y = random.randint(50, 600)
+                bug = Bug(x, y)
+                # all_bugs.add(bug)
                 all_bugs.append(bug)
         for um_bug in all_bugs:
             um_bug.trace(screen)
             um_bug.update(player)
-        
-        #all_bugs.draw(screen)
-        #all_bugs.update(player)
 
-        #Destruindo os projéteis e os bugs quando entram em colisão
+        # all_bugs.draw(screen)
+        # all_bugs.update(player)
+
+        # Destruindo os projéteis e os bugs quando entram em colisão
         remove_bullets = []
         remove_bugs = []
         for bala in all_bullets:
             bala.destroy = False
-            if (bala.rect.x < 0 or bala.rect.x > width) or (bala.rect.y < 0 or bala.rect.y > height): #Condicional para o projétil ser removido quando off-screen
+            # Condicional para o projétil ser removido quando off-screen
+            if (bala.rect.x < 0 or bala.rect.x > width) or (bala.rect.y < 0 or bala.rect.y > height):
                 all_bullets.remove(bala)
                 print('removi bala')
             for um_bug in all_bugs:
@@ -166,17 +171,18 @@ def main():
                         if um_bug not in remove_bugs:
                             remove_bugs.append(um_bug)
 
-                        itens_coletados['bugs']+= 1
-                        
+                        itens_coletados['bugs'] += 1
 
         for bala in remove_bullets:
             all_bullets.remove(bala)
+
         for um_bug in remove_bugs:
             all_bugs.remove(um_bug)
-            #Tem uma chance de gerar um item no lugar onde o bug morre
-            if random.randint(0, 2) == 1:
-              gerar_itens(itens_lista, all_items, player,um_bug.x,um_bug.y)
+            morte_bug_sound.play()  # Efeito sonoro da coleta de item
 
+            # Tem uma chance de gerar um item no lugar onde o bug morre
+            if random.randint(0, 2) == 1:
+                gerar_itens(itens_lista, all_items, player, um_bug.x, um_bug.y)
 
         # Inserir os itens coletados,bugs mortos e a pontuação na tela
         text_coffee = font_game.render(
@@ -184,16 +190,16 @@ def main():
         text_energy_drink = font_game.render(
             f'X {itens_coletados["energy_drink"]}', 1, branco)
         text_bugs = font_game.render(
-            f'X {itens_coletados["bugs"]}', 1,branco)
-        #Pontuação:Um bug  vale 1 ponto e cada bit vale 5 pontos
+            f'X {itens_coletados["bugs"]}', 1, branco)
+        # Pontuação:Um bug  vale 1 ponto e cada bit vale 5 pontos
         text_pontuacao = font_game.render(
             f'Pontuação: {(itens_coletados["bit_0"] + itens_coletados["bit_1"])*5 + itens_coletados["bugs"]}', 1, branco)
-        
-        screen.blit(text_pontuacao,(270, 10))
+
+        screen.blit(text_pontuacao, (270, 10))
         screen.blit(pg.transform.scale(pg.image.load(
-            'assets\\bug_simples.png'), (40,35)),(20,105))
-        screen.blit(text_bugs,(70,115))
-        
+            'assets\\bug_simples.png'), (40, 35)), (20, 105))
+        screen.blit(text_bugs, (70, 115))
+
         screen.blit(pg.transform.scale(pg.image.load(
             'assets\cafe.gif'), (40, 35)), (20, 20))
         screen.blit(text_coffee, (70, 35))
@@ -201,9 +207,8 @@ def main():
             'assets\energy_drink.png'), (35, 35)), (25, 65))
         screen.blit(text_energy_drink, (70, 75))
 
-        for balas in all_bullets: #desenha o projetil gas na tela
+        for balas in all_bullets:  # desenha o projetil gas na tela
             balas.trace(screen)
-
 
         # Atualiza o dicionário de itens coletados
         for i in itens_lista:
@@ -212,15 +217,15 @@ def main():
                 itens_coletados[coletado] += 1
                 print(itens_coletados)
 
-                item_sound.play() # Efeito sonoro
+                item_sound.play()  # Efeito sonoro da coleta de item
 
         pg.display.flip()
         clock.tick(30)
         contador += 1
 
-
         # Testando a coleta de itens
         keys = pg.key.get_pressed()
+
 
 if __name__ == '__main__':
     main()
