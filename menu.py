538f844111e746fe13b76039ada971f8a611f3e8
Ykaro dos Santos <110127975+ykarocin@users.noreply.github.com>
2022-10-27 20:07:19 -0300

    som para o menu

--- menu.py
+++ menu.py
@@ -9,6 +9,10 @@ from main import main
 def menu_screen():
 
     pg.init()
+
+    pg.mixer.music.load(Path('assets','menu_sound.mp3'))
+    pg.mixer.music.set_volume(0.7)
+    pg.mixer.music.play(-1)
     
     botoes = []
 
